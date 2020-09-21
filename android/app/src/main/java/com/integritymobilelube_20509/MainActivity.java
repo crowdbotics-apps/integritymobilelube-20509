1a050745abeddcdf157c491f685f01accddfec7d
Crowdbotics <team@crowdbotics.com>
2020-09-21 16:54:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/integritymobilelube_20509/MainActivity.java
@@ -0,0 +1,15 @@
+package com.integritymobilelube_20509;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "integritymobilelube_20509";
+    }
+}
